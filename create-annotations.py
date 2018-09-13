6ee3cbbeece1a597c52b392ef024de6466ff92dd
Adam Richard-Bollans <mm15alrb@leeds.ac.uk>
2018-09-13 11:00:37 +0100

    Add files via upload

--- create-annotations.py
+++ create-annotations.py
@@ -17,9 +17,9 @@ class Annotation():
 	def __str__(self):
 		return "["+str(self.scene)+","+str(self.preposition)+"]"
 
-x= raw_input("What is the number of the scene you would like to annotate?") #set x to be random integer within range of files
-task_type = raw_input("What kind of task would you like to do? s = Standard annotation task, hfg = Task where a figure and ground is given and preposition must be selected")
-scenefile = "scene"+str(x)+"-"+task_type+".blend"
+#x= raw_input("What is the number of the scene you would like to annotate?") #set x to be random integer within range of files
+#task_type = raw_input("What kind of task would you like to do? s = Standard annotation task, hfg = Task where a figure and ground is given and preposition must be selected")
+scenefile = raw_input("Which scene file would you like to annotate?") #"scene"+str(x)+"-"+task_type+".blend"
 
 ### run annotation game
 
