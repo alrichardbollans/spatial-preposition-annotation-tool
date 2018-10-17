df6f0f9645fccf745a90c3ea81ad81029b047b92
Adam Richard-Bollans <mm15alrb@leeds.ac.uk>
2018-10-17 14:59:27 +0100

    Add files via upload
    Created a UI for starting the program

--- create-annotations.py
+++ create-annotations.py
@@ -1,90 +1,176 @@
+from Tkinter import *
+
 import os
 import csv
 import uuid
 
-### User Info
-user_id = uuid.uuid4()
-age = raw_input('Age: ')
-gender = raw_input('Gender: ')
-city = raw_input('Where were you born? ')
-
-
-
-with open('outputs/user list.csv', "a+") as csvfile:
-	writer = csv.writer(csvfile)
-	writer.writerow([user_id,age,gender,city])
-
-
 class Annotation():
-	preposition = 0
-	scene = 0
-	figure = 0
-	ground = 0
-	cam_loc = []
-	cam_rot = []
+    preposition = 0
+    scene = 0
+    figure = 0
+    ground = 0
+    cam_loc = []
+    cam_rot = []
 
-	def __init__(self):
-		self.id = uuid.uuid4()
-	def __str__(self):
-		return "["+str(self.scene)+","+str(self.preposition)+"]"
+    def __init__(self):
+        self.id = uuid.uuid4()
 
-#x= raw_input("What is the number of the scene you would like to annotate?") #set x to be random integer within range of files
-#task_type = raw_input("What kind of task would you like to do? s = Standard annotation task, hfg = Task where a figure and ground is given and preposition must be selected")
-sceneinput = raw_input("Which scene file would you like to annotate? (Only the file name is necessary, not the path)") #"scene"+str(x)+"-"+task_type+".blend"
-if ".blend" in sceneinput:
-	scenefile = sceneinput
-	scene = scenefile[:scenefile.find(".blend")]
-else:
-	scene = sceneinput
-	scenefile = sceneinput + ".blend"
+    def __str__(self):
+        return "["+str(self.scene)+","+str(self.preposition)+"]"
 
-task_type = scenefile[scenefile.find("-")+ 1:scenefile.find(".blend")] ### task type is between "-" and ".blend"
+class Application(Frame):
 
-### run annotation game
+    def createWidgets(self):
+        self.winfo_toplevel().title("Annotation Tool")
 
-os.system("./scene\ creation/blender/annotation\ scenes/" +str(scenefile)) 
+        self.label0 = Label(self, text='Select Scene:')
+        self.label0.grid(row=0)
+        
+        self.variable=StringVar()
+        self.variable.set(get_scene_files()[0])
 
-### create list from output of tool
-with open('output.csv', "r+") as f: 
-	reader = csv.reader(f)     
-	datalist = list( reader )  
+        self.SCENE=apply(OptionMenu, (self, self.variable) + tuple(get_scene_files()))
+        self.SCENE.grid(row=0,column=1)
 
 
-
-for annotation in datalist:
-	an = Annotation()
-	an.task = str(task_type)
-	an.preposition = str(annotation[1])
-	an.scene = scene
-	an.figure = str(annotation[0])
-	an.ground = str(annotation[2])
-	an.cam_loc = str(annotation[4])
-	an.cam_rot = str(annotation[5])
-
-	### add annotations to list
-	with open('outputs/annotation list.csv', "a+") as csvfile:
-		writer = csv.writer(csvfile)
-		writer.writerow([user_id,an.task,an.scene,an.preposition,an.figure,an.ground,an.cam_loc,an.cam_rot])
-
-### delete output from tool
-
-os.system("rm output.csv")
+        self.AGE = Entry(self)
+        self.AGE.grid(row=1, column=1)
+        self.AGE.focus_set()
 
 
+        self.label1 = Label(self, text="Age:")
+        self.label1.grid(row=1)
 
+        self.POB = Entry(self)
+        self.POB.grid(row=2, column=1)
+
+        self.label2 = Label(self, text="Place of Birth:")
+        self.label2.grid(row=2)
+
+        self.GENDER = Entry(self)
+        self.GENDER.grid(row=3, column=1)
+
+        self.label3 = Label(self, text="Gender:")
+        self.label3.grid(row=3)
+
+        self.START = Button(self, text="START", fg='blue', command=self.start)
+        self.START.grid(row=4, column=0)
+
+
+        self.QUIT = Button(self, text="QUIT", fg='red', command=self.quit)
+        self.QUIT.grid(row=4,column=1)
 
 
 
 
-# for instance in datalist:
-# 	if instance.scene == "currentscene":
-# 		instance.scene = "undefined scene"
+    def retrieve_user_info(self):
+        global user_id
+        user_id = uuid.uuid4()
+        return [user_id,self.AGE.get(),self.POB.get(),self.GENDER.get()]
+    def retrieve_scene_file(self):
+        return self.variable.get()
+    def retrieve_scene_name(self):
 
-# f = open('/scenes/output.csv', "r+")
-# if csv[3] == "currentscene":
-# 	csv[3] = "undefined scene"
+        file = self.retrieve_scene_file()
+        return file[:file.find(".blend")]
+    def retrieve_task_type(self):
+        file = self.retrieve_scene_file()
+        return file[file.find("-")+ 1:file.find(".blend")] ### task type is between "-" and ".blend"
+
+    def start(self):
+        self.write_user_info()
+        self.run_game()
+        self.write_annotations(output_path)
 
 
-# f = open('/scenes/output.csv', "r+")
-# if csv[3] == "currentscene":
-# 	csv[3] = scenefile
+    def run_game(self):
+        os.system("./scene\ creation/blender/annotation\ scenes/" +self.retrieve_scene_file())
+
+    def write_user_info(self):
+        with open(output_path + 'user list.csv', "a+") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(self.retrieve_user_info())
+
+    def write_annotations(self,output_path):
+        ### create list from output of tool
+        with open('output.csv', "r+") as f: 
+            reader = csv.reader(f)     
+            datalist = list( reader )  
+
+
+
+        for annotation in datalist:
+            an = Annotation()
+            an.task = self.retrieve_task_type()
+            an.preposition = str(annotation[1])
+            an.scene = self.retrieve_scene_name()
+            an.figure = str(annotation[0])
+            an.ground = str(annotation[2])
+            an.cam_loc = str(annotation[4])
+            an.cam_rot = str(annotation[5])
+
+            ### add annotations to list
+            with open(output_path + 'annotation list.csv', "a+") as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow([an.id,user_id,an.task,an.scene,an.preposition,an.figure,an.ground,an.cam_loc,an.cam_rot])
+
+        ### delete output from tool
+
+        os.system("rm output.csv")
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+def dropbox_home():
+    from platform import system
+    import base64
+    import os.path
+    _system = system()
+    if _system in ('Windows', 'cli'):
+        host_db_path = os.path.join(_get_appdata_path(),
+                                    'Dropbox',
+                                    'host.db')
+    elif _system in ('Linux', 'Darwin'):
+        host_db_path = os.path.expanduser('~'
+                                          '/.dropbox'
+                                          '/host.db')
+    else:
+        raise RuntimeError('Unknown system={}'
+                           .format(_system))
+    if not os.path.exists(host_db_path):
+        raise RuntimeError("Config path={} doesn't exists"
+                           .format(host_db_path))
+    with open(host_db_path, 'r') as f:
+        data = f.read().split()
+
+    return base64.b64decode(data[1])
+
+
+
+
+
+
+
+def get_scene_files():
+    files = []
+    for filename in os.listdir('scene creation/blender/annotation scenes/'):
+        if filename.endswith(".blend"):
+            files.append(filename)
+    return files
+
+
+
+dropbox_path = dropbox_home()
+output_path = 'test/'
+
+
+
+
+root = Tk()
+app = Application(master=root)
+
+app.mainloop()
+
+root.destroy()
