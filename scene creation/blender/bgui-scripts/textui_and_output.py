57a2da56b7ad81270609380ea5eeb8428e84653c
Adam Richard-Bollans <mm15alrb@leeds.ac.uk>
2018-10-30 17:38:05 +0000

    Add files via upload
    Fixed some bugs

--- scene creation/blender/bgui-scripts/textui_and_output.py
+++ scene creation/blender/bgui-scripts/textui_and_output.py
@@ -26,6 +26,7 @@ deselect = co.sensors["deselect"]
 
 if deselect.positive:
     empty['confirm'] = False
+    co.owner['sys'].layout.confirmlbl.visible = False
 
 keyboard = co.sensors["textinputkeyboard"]
 
@@ -62,47 +63,89 @@ class SimpleLayout(bgui.bge_utils.Layout):
 
         # Add a label for the title
         self.titlelbl = bgui.Label(self, text=empty.getPropertyNames()[0], pos=[0, .95],
-            sub_theme='Large', options = bgui.BGUI_DEFAULT | bgui.BGUI_CENTERX) 
-        # Add a label for the figure
-        self.figurelbl = bgui.Label(self, text='Figure: ',pos=[.25, .9],
-            sub_theme='Large', options = bgui.BGUI_DEFAULT)
-        # Add a label for the ground
-        self.groundlbl = bgui.Label(self, text='Ground: ', pos=[.7, .9],
-            sub_theme='Large', options = bgui.BGUI_DEFAULT)
+            sub_theme='Large', pt_size = 35,options = bgui.BGUI_DEFAULT | bgui.BGUI_CENTERX)
 
-        self.confirmlbl = bgui.Label(self, text='Press Enter to Confirm this selection', pos=[0, .8],
-            sub_theme='Large', options = bgui.BGUI_DEFAULT | bgui.BGUI_CENTERX)
-        self.confirmlbl.visible = False
-        if "p" not in empty.getPropertyNames():
-            # A label for the given preposition
-            self.prepositionlbl = bgui.Label(self, text= 'Preposition: '+preposition_list[0], pos=[0, 0.9],
-                sub_theme='Large', options = bgui.BGUI_DEFAULT | bgui.BGUI_CENTERX)
+        if 'pragmatic' not in empty.getPropertyNames():
+            # Add a label for the figure
+            self.figurelbl = bgui.Label(self, text='Figure: ',pos=[.25, .9],
+                sub_theme='Large', options = bgui.BGUI_DEFAULT)
+            # Add a label for the ground
+            self.groundlbl = bgui.Label(self, text='Ground: ', pos=[.7, .9],
+                sub_theme='Large', options = bgui.BGUI_DEFAULT)
 
+            self.confirmlbl = bgui.Label(self, text='Press Enter to Confirm this selection', pos=[0, .5],
+                sub_theme='Large', color = (0,1,0,1),outline_color=(1,0,0,1), outline_size = 2, pt_size = 50, options = bgui.BGUI_DEFAULT | bgui.BGUI_CENTERX)
+            self.confirmlbl.visible = False
 
+            if "p" not in empty.getPropertyNames():
+                # A label for the given preposition
+                self.prepositionlbl = bgui.Label(self, text= 'Preposition: '+preposition_list[0], pos=[0, 0.9],
+                    sub_theme='Large', options = bgui.BGUI_DEFAULT | bgui.BGUI_CENTERX)
         
-        if "p" in empty.getPropertyNames():
-            # A themed frame to store widgets
-            self.win = bgui.Frame(self,  size=[.6, .1],pos=[0, 0.1],border=0.2,
-                options=bgui.BGUI_DEFAULT|bgui.BGUI_CENTERX)
+            if "p" in empty.getPropertyNames():
+                # A themed frame to store widgets
+                self.win = bgui.Frame(self,  size=[.6, .1],pos=[0, 0.1],border=0.2,
+                    options=bgui.BGUI_DEFAULT|bgui.BGUI_CENTERX) 
 
-            self.prepositionlbl = bgui.Label(self, text="", pos=[0, 0.9],
-                sub_theme='Large', options = bgui.BGUI_DEFAULT | bgui.BGUI_CENTERX)
+                self.prepositionlbl = bgui.Label(self, text="", pos=[0, 0.9],
+                    sub_theme='Large', options = bgui.BGUI_DEFAULT | bgui.BGUI_CENTERX)
 
+                # A TextInput widget
+                self.input = bgui.TextInput(self.win, text="",
+                    input_options = 1, options = bgui.BGUI_DEFAULT)
+                self.input.activate()
+                self.input.on_enter_key = self.on_input_enter
+        
+        if 'pragmatic' in empty.getPropertyNames():
             # A TextInput widget
             self.input = bgui.TextInput(self.win, text="",
                 input_options = 1, options = bgui.BGUI_DEFAULT)
             self.input.activate()
             self.input.on_enter_key = self.on_input_enter
 
-
-    def on_input_enter(self, widget):
-        if empty.get('confirm') == False:
-            self.prepositionlbl.text = "You've entered: " + widget.text
-            widget.text = ""
-        else:
-            pass
+            if 'f' in empty.getPropertyNames():
+                # Add a label for the figure
+                self.figurelbl = bgui.Label(self, text='Figure: ',pos=[.25, .9],
+                    sub_theme='Large', options = bgui.BGUI_DEFAULT)
 
         
+
+
+        
+
+
+
+    def on_input_enter(self, widget):
+        # in pragmatic tasks just output immediately
+        if "pragmatic" in empty.getPropertyNames():
+            triple=[0,1,2]
+            triple[1] = widget.text
+            if 'f' in empty.getPropertyNames():
+                for obj in selectable_objects:
+                    
+                    if obj.get('selectedfigure')==True:
+                        triple[0]=obj
+                
+                triple[2] = 'N/A'
+
+            bge.logic.sendMessage("deselect") #Sends a deselect message to sensors in any active scene.
+            bge.logic.sendMessage("change")
+
+            print(str(triple))
+            triple.append("currentscene")
+
+            cam_loc = main_scene.objects["Camera"].position
+            cam_rot = main_scene.objects["Camera"].orientation
+            triple.append(cam_loc)
+            triple.append(cam_rot)
+
+            with open('output.csv', "a") as csvfile:
+                outputwriter = csv.writer(csvfile)
+                outputwriter.writerow(triple)
+            
+        elif empty.get('confirm') == False:
+            self.prepositionlbl.text = "You've entered: " + widget.text
+        widget.text = ""
 
 
         
@@ -138,130 +181,141 @@ def main(cont):
 
 main(bge.logic.getCurrentController())
 
+if 'pragmatic' not in empty.getPropertyNames():
+    for obj in selectable_objects:
+        if obj.get('selectedfigure')==True:
+            co.owner['sys'].layout.figurelbl.text = 'Figure: '+ obj.name
 
-for obj in selectable_objects:
-    if obj.get('selectedfigure')==True:
-        co.owner['sys'].layout.figurelbl.text = 'Figure: '+ obj.name
-
-    if obj.get('selectedground')==True: 
-        co.owner['sys'].layout.groundlbl.text = 'Ground: '+ obj.name
-
-
-
-if all(obj['selectedfigure']==False for obj in selectable_objects):
-    co.owner['sys'].layout.figurelbl.text = 'Figure: '
-
-if all(obj['selectedground']==False for obj in selectable_objects):
-    co.owner['sys'].layout.groundlbl.text = 'Ground: '
+        if obj.get('selectedground')==True: 
+            co.owner['sys'].layout.groundlbl.text = 'Ground: '+ obj.name
 
 
 
-if "p" in empty.getPropertyNames():
+    if all(obj['selectedfigure']==False for obj in selectable_objects):
+        co.owner['sys'].layout.figurelbl.text = ''
+
+    if all(obj['selectedground']==False for obj in selectable_objects):
+        co.owner['sys'].layout.groundlbl.text = ''
+
+    if "p" in empty.getPropertyNames():
+        ###Keep text input focused
+        co.owner['sys'].layout.input.system.focused_widget = co.owner['sys'].layout.input
+
+        triple=[0,1,2]
+        triple[1] = co.owner['sys'].layout.prepositionlbl.text.replace("You've entered: ",'')
+        for obj in selectable_objects:
+            
+            if obj.get('selectedfigure')==True:
+                triple[0]=obj
+
+            if obj.get('selectedground')==True: 
+                triple[2] = obj
+
+        if triple[0] != 0 and triple[2] != 2 and triple[1] != '':
+            co.owner['sys'].layout.confirmlbl.visible = True
+
+            # the confirm property is necessary to force the script to have to run a second time
+            if empty.get('confirm') == True:
+                for key,status in keyboard.events:
+                    # key[0] == bge.events.keycode, key[1] = status
+                    if status == bge.logic.KX_INPUT_JUST_ACTIVATED:
+                        if key == bge.events.ENTERKEY:
+                            bge.logic.sendMessage("deselect") #Sends a deselect message to sensors in any active scene.
+                            bge.logic.sendMessage("change")
+
+                            print(str(triple))
+                            triple.append("currentscene")
+
+                            cam_loc = main_scene.objects["Camera"].position
+                            cam_rot = main_scene.objects["Camera"].orientation
+                            triple.append(cam_loc)
+                            triple.append(cam_rot)
+
+                            with open('output.csv', "a") as csvfile:
+                                outputwriter = csv.writer(csvfile)
+                                outputwriter.writerow(triple)
+
+
+                            co.owner['sys'].layout.prepositionlbl.text = ''
+
+                            co.owner['sys'].layout.confirmlbl.visible = False
+
+            empty['confirm'] = True
+        # if triple[1] == '':
+        #     co.owner['sys'].layout.input.on_enter_key = co.owner['sys'].layout.on_input_enter(co.owner['sys'].layout.input)
+    if "p" not in empty.getPropertyNames():
+        #### Output annotations
+        triple=[0,1,2]
+        triple[1] = co.owner['sys'].layout.prepositionlbl.text.replace('Preposition: ','')
+        for obj in main_scene.objects:
+            
+            if obj.get('selectedfigure')==True:
+                triple[0]=obj
+
+            if obj.get('selectedground')==True: 
+                triple[2] = obj
+
+        if triple[0] != 0 and triple[2] != 2:
+            co.owner['sys'].layout.confirmlbl.visible = True
+
+            if empty.get('confirm') == True:
+                for key,status in keyboard.events:
+                    # key[0] == bge.events.keycode, key[1] = status
+                    if status == bge.logic.KX_INPUT_JUST_ACTIVATED:
+                        if key == bge.events.ENTERKEY:
+                            bge.logic.sendMessage("deselect") #Sends a deselect message to sensors in any active scene.
+                            bge.logic.sendMessage("change")
+
+                            print(str(triple))
+                            triple.append("currentscene")
+
+                            cam_loc = main_scene.objects["Camera"].position
+                            cam_rot = main_scene.objects["Camera"].orientation
+                            triple.append(cam_loc)
+                            triple.append(cam_rot)
+
+                            with open('output.csv', "a") as csvfile:
+                                outputwriter = csv.writer(csvfile)
+                                outputwriter.writerow(triple)
+
+
+                            
+
+                            co.owner['sys'].layout.confirmlbl.visible = False
+                            
+            empty['confirm'] = True
+
+
+        #### Change preposition
+        changeprep = co.sensors["changepreposition"]
+
+        for key,status in keyboard.events:
+            # key[0] == bge.events.keycode, key[1] = status
+            if status == bge.logic.KX_INPUT_JUST_ACTIVATED:
+                if key == bge.events.SPACEKEY:
+                    bge.logic.sendMessage("changepreposition")
+
+        if changeprep.positive:
+            co.owner['sys'].layout.prepositionlbl.text = 'Preposition: ' + random.choice(preposition_list)
+            # new_index = preposition_list.index(co.owner['sys'].layout.lbl2.text) + 1
+
+            # if new_index < len(preposition_list):
+
+            #     co.owner['sys'].layout.lbl2.text = preposition_list[new_index]
+            # else:
+            #     co.owner['sys'].layout.lbl2.text = preposition_list[0]
+if "pragmatic" in empty.getPropertyNames():
+    ###Keep text input focused
     co.owner['sys'].layout.input.system.focused_widget = co.owner['sys'].layout.input
 
-    triple=[0,1,2]
-    triple[1] = co.owner['sys'].layout.prepositionlbl.text.replace("You've entered: ",'')
-    for obj in selectable_objects:
-        
-        if obj.get('selectedfigure')==True:
-            triple[0]=obj
-
-        if obj.get('selectedground')==True: 
-            triple[2] = obj
-
-    if triple[0] != 0 and triple[2] != 2 and triple[1] != '':
-        co.owner['sys'].layout.confirmlbl.visible = True
-
-        if empty.get('confirm') == True:
-            for key,status in keyboard.events:
-                # key[0] == bge.events.keycode, key[1] = status
-                if status == bge.logic.KX_INPUT_JUST_ACTIVATED:
-                    if key == bge.events.ENTERKEY:
-                        bge.logic.sendMessage("deselect") #Sends a deselect message to sensors in any active scene.
-                        bge.logic.sendMessage("change")
-
-                        print(str(triple))
-                        triple.append("currentscene")
-
-                        cam_loc = main_scene.objects["Camera"].position
-                        cam_rot = main_scene.objects["Camera"].orientation
-                        triple.append(cam_loc)
-                        triple.append(cam_rot)
-
-                        with open('output.csv', "a") as csvfile:
-                            outputwriter = csv.writer(csvfile)
-                            outputwriter.writerow(triple)
+    if 'f' in empty.getPropertyNames():
+        for obj in selectable_objects:
+            if obj.get('selectedfigure')==True:
+                co.owner['sys'].layout.figurelbl.text = 'Figure: '+ obj.name
 
 
-                        co.owner['sys'].layout.prepositionlbl.text = ''
-
-                        co.owner['sys'].layout.confirmlbl.visible = False
-                        empty['confirm'] = False
-        empty['confirm'] = True
-    # if triple[1] == '':
-    #     co.owner['sys'].layout.input.on_enter_key = co.owner['sys'].layout.on_input_enter(co.owner['sys'].layout.input)
-
-if "p" not in empty.getPropertyNames():
-    #### Output annotations
-    triple=[0,1,2]
-    triple[1] = co.owner['sys'].layout.prepositionlbl.text.replace('Preposition: ','')
-    for obj in main_scene.objects:
-        
-        if obj.get('selectedfigure')==True:
-            triple[0]=obj
-
-        if obj.get('selectedground')==True: 
-            triple[2] = obj
-
-    if triple[0] != 0 and triple[2] != 2:
-        co.owner['sys'].layout.confirmlbl.visible = True
-
-        if empty.get('confirm') == True:
-            for key,status in keyboard.events:
-                # key[0] == bge.events.keycode, key[1] = status
-                if status == bge.logic.KX_INPUT_JUST_ACTIVATED:
-                    if key == bge.events.ENTERKEY:
-                        bge.logic.sendMessage("deselect") #Sends a deselect message to sensors in any active scene.
-                        bge.logic.sendMessage("change")
-
-                        print(str(triple))
-                        triple.append("currentscene")
-
-                        cam_loc = main_scene.objects["Camera"].position
-                        cam_rot = main_scene.objects["Camera"].orientation
-                        triple.append(cam_loc)
-                        triple.append(cam_rot)
-
-                        with open('output.csv', "a") as csvfile:
-                            outputwriter = csv.writer(csvfile)
-                            outputwriter.writerow(triple)
 
 
-                        co.owner['sys'].layout.prepositionlbl.text = ''
-
-                        co.owner['sys'].layout.confirmlbl.visible = False
-                        empty['confirm'] = False
-        empty['confirm'] = True
-
-
-    #### Change preposition
-    changeprep = co.sensors["changepreposition"]
-
-    for key,status in keyboard.events:
-        # key[0] == bge.events.keycode, key[1] = status
-        if status == bge.logic.KX_INPUT_JUST_ACTIVATED:
-            if key == bge.events.SPACEKEY:
-                bge.logic.sendMessage("changepreposition")
-
-    if changeprep.positive:
-        co.owner['sys'].layout.prepositionlbl.text = 'Preposition: ' + random.choice(preposition_list)
-        # new_index = preposition_list.index(co.owner['sys'].layout.lbl2.text) + 1
-
-        # if new_index < len(preposition_list):
-
-        #     co.owner['sys'].layout.lbl2.text = preposition_list[new_index]
-        # else:
-        #     co.owner['sys'].layout.lbl2.text = preposition_list[0]
 
 
 
